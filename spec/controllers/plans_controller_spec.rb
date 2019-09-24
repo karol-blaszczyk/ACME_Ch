472953beff628ff6c97e365b810057d26712dc84
karol-blaszczyk <karol.zygmunt.blaszczyk@gmail.com>
2019-09-24 17:21:49 +0200

    plans controller

--- spec/controllers/plans_controller_spec.rb
+++ spec/controllers/plans_controller_spec.rb
@@ -26,102 +26,12 @@ require 'rails_helper'
 # `rails-controller-testing` gem.
 
 RSpec.describe PlansController, type: :controller do
-  # This should return the minimal set of attributes required to create a valid
-  # Plan. As you add validations to Plan, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) do
-    skip('Add a hash of attributes valid for your model')
-  end
-
-  let(:invalid_attributes) do
-    skip('Add a hash of attributes invalid for your model')
-  end
-
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # PlansController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+  let(:plan) { create(:plan) }
 
   describe 'GET #index' do
     it 'returns a success response' do
-      plan = Plan.create! valid_attributes
-      get :index, params: {}, session: valid_session
+      get :index
       expect(response).to be_successful
-    end
-  end
-
-  describe 'GET #show' do
-    it 'returns a success response' do
-      plan = Plan.create! valid_attributes
-      get :show, params: { id: plan.to_param }, session: valid_session
-      expect(response).to be_successful
-    end
-  end
-
-  describe 'POST #create' do
-    context 'with valid params' do
-      it 'creates a new Plan' do
-        expect do
-          post :create, params: { plan: valid_attributes }, session: valid_session
-        end.to change(Plan, :count).by(1)
-      end
-
-      it 'renders a JSON response with the new plan' do
-        post :create, params: { plan: valid_attributes }, session: valid_session
-        expect(response).to have_http_status(:created)
-        expect(response.content_type).to eq('application/json')
-        expect(response.location).to eq(plan_url(Plan.last))
-      end
-    end
-
-    context 'with invalid params' do
-      it 'renders a JSON response with errors for the new plan' do
-        post :create, params: { plan: invalid_attributes }, session: valid_session
-        expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to eq('application/json')
-      end
-    end
-  end
-
-  describe 'PUT #update' do
-    context 'with valid params' do
-      let(:new_attributes) do
-        skip('Add a hash of attributes valid for your model')
-      end
-
-      it 'updates the requested plan' do
-        plan = Plan.create! valid_attributes
-        put :update, params: { id: plan.to_param, plan: new_attributes }, session: valid_session
-        plan.reload
-        skip('Add assertions for updated state')
-      end
-
-      it 'renders a JSON response with the plan' do
-        plan = Plan.create! valid_attributes
-
-        put :update, params: { id: plan.to_param, plan: valid_attributes }, session: valid_session
-        expect(response).to have_http_status(:ok)
-        expect(response.content_type).to eq('application/json')
-      end
-    end
-
-    context 'with invalid params' do
-      it 'renders a JSON response with errors for the plan' do
-        plan = Plan.create! valid_attributes
-
-        put :update, params: { id: plan.to_param, plan: invalid_attributes }, session: valid_session
-        expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to eq('application/json')
-      end
-    end
-  end
-
-  describe 'DELETE #destroy' do
-    it 'destroys the requested plan' do
-      plan = Plan.create! valid_attributes
-      expect do
-        delete :destroy, params: { id: plan.to_param }, session: valid_session
-      end.to change(Plan, :count).by(-1)
     end
   end
 end
