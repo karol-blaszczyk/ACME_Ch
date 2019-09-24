3e7b31fb9779ebd49a71a2004e002eac7acbf007
karol-blaszczyk <karol.zygmunt.blaszczyk@gmail.com>
2019-09-24 17:30:48 +0200

    plan, subscription routes and plan seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  resources :plans
-  resources :subscriptions
+  resources :plans, only: :index
+  resources :subscriptions, only: %i[index show create destroy]
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+[{
+  name: 'Bronze Box',
+  price: 1999
+},
+ {
+   name: 'Silver Box',
+   price: 4900
+
+ },
+ {
+   name: 'Gold Box',
+   price: 9900
+
+ }].each { |plan| Plan.create(plan) }
