398dc8857ac9c0dce9b2cc3bd035139ef7c09e2a
karol-blaszczyk <karol.zygmunt.blaszczyk@gmail.com>
2019-09-25 12:07:40 +0200

    move container to separate file

--- app/services/create_subscription.rb
+++ app/services/create_subscription.rb
@@ -1,33 +1,6 @@
 # frozen_string_literal: true
 
 require 'dry/transaction'
-require 'dry/container'
-
-class Container
-  extend Dry::Container::Mixin
-
-  namespace 'subscriptions' do
-    register 'charge_card' do
-      Subscriptions::Charge.new
-    end
-
-    register 'save' do
-      Subscriptions::Save.new
-    end
-
-    register 'save_shipping_information' do
-      Subscriptions::SaveShippingInformation.new
-    end
-
-    register 'validate' do
-      Subscriptions::Validate.new
-    end
-
-    register 'set_billing_day' do
-      Subscriptions::SetBillingDay.new
-    end
-  end
-end
 
 class CreateSubscription
   include Dry::Transaction(container: ::Container)
